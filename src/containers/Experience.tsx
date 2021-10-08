62517c5569596a1024fce48578ea7c953437b333
iamalanxue <alanxue43@gmail.com>
2021-10-08 14:28:15 -0500

    implemented logic for experience seciton

--- src/containers/Experience.tsx
+++ src/containers/Experience.tsx
@@ -1,18 +1,47 @@
-import { Typography, Grid } from '@mui/material'; 
+import { Typography, Grid, Link } from '@mui/material'; 
 import IEEE from '../common/img/ieee.jpg';
 import Relayr from '../common/img/relayr.jpg'; 
 import Item from '../components/Item'; 
 
+
 const Experience = () => {
+
   return (
     <div className="experience">
       <Typography variant="h1">
         Current Projects 
       </Typography>
       <div className="border"></div>
-      <div className="container">
-        <Item xs={6} md={5} xs2={6} md2={7} img={IEEE}/>
-        <Item xs={6} md={5} xs2={6} md2={7} img={Relayr}/>
+      <Typography variant="caption">My journey to transition from an Engineer to a Software Developer 😄. More to come!</Typography>
+      <div id="experience-container">
+      <Grid container spacing={3} wrap="wrap">
+        <Grid item xs={6} md={4}>
+          <div className="item-img"> 
+            <img src={Relayr} alt="Relayr Logo" />
+          </div>
+        </Grid>
+        <Grid item xs={6} md={7}>
+          <Link href="https://relayr.io/" underline="none" target="_blank" rel="noopener">Relayr Inc. </Link>
+          <br />
+          <Typography>Full Stack Developer Intern, Jun 2021 - Sep 2021</Typography>
+          <Typography variant="caption">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem consequuntur quibusdam provident doloribus recusandae porro nulla est ratione? Consequatur corporis at quia sint suscipit, totam eius fuga recusandae quidem veniam!
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={4}>
+          <div className="item-img">
+            <img src={IEEE} alt="IEEE Logo" />
+          </div>
+        </Grid>
+        <Grid item xs={6} md={7}>
+          <Link href="https://ieee.northwestern.edu/" underline="none" target="_blank"    rel="noopener">Institute of Electrical and Electronics Engineers</Link>
+          <br />
+          <Typography>Machine Learning BackEnd Lead Member, Dec 2021 - Jun 2021</Typography>
+          <Typography variant="caption">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem consequuntur quibusdam provident doloribus recusandae porro nulla est ratione? Consequatur corporis at quia sint suscipit, totam eius fuga recusandae quidem veniam!
+          </Typography>
+        </Grid>
+      </Grid>
       </div>
     </div>
   )
